e8498ba882caf9e7c83a4eb911a15d89c587d939
Tofandel <adrien.foulon@tukan.hu>
2019-11-01 16:55:08 +0100

    Linting + move away from css scoping (heavy dom)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,21 +2,20 @@
     <div class="header-container" :style="{background: colors.header.bg}">
         <slot name="header" :colors="colors" :chatTitle="chatTitle" :participants="participants" :myself="myself"></slot>
         <div v-if="!hasHeaderSlot" class="header-title">
-
-            <p class="header-title-text" :style="{color: colors.header.text}">{{ chatTitle }}</p>
+            <p class="header-title-text" :style="{color: colors.header.text}">{{chatTitle}}</p>
             <p class="header-paticipants-text">
                 <span>
-                    {{ `${myself.name}, ` }}
+                    {{`${myself.name}, `}}
                 </span>
-                <span v-for="(participant, index) in participants" :key="participant.id" >
-                    {{ participants.length-1 != index?`${participant.name}, ` : participant.name }}
+                <span v-for="(participant, index) in participants" :key="participant.id">
+                    {{participants.length-1 != index?`${participant.name}, ` : participant.name}}
                 </span>
             </p>
         </div>
 
         <div v-if="!hideCloseButton && !hasHeaderSlot" class="header-exit">
             <slot name="close-button" :onClose="onClose">
-                <a class="header-exit-button" href="#" @click.prevent="onClose" :style="{fontSize: closeButtonIconSize}">✕</a>
+                <a class="header-exit-button" href="#" :style="{fontSize: closeButtonIconSize}" @click.prevent="onClose">✕</a>
             </slot>
         </div>
     </div>
@@ -76,48 +75,49 @@
     }
 </script>
 
-<style scoped>
-    .header-container{
+<style lang="less">
+    .quick-chat-container .header-container {
         /* background: #d30303; */
         height: 70px;
         display: flex;
         padding: 0 20px 0 10px;
         align-items: center;
-        -webkit-box-shadow:  0 2px 20px 2px rgba(90, 90, 90, 0.47);
+        -webkit-box-shadow: 0 2px 20px 2px rgba(90, 90, 90, 0.47);
         box-shadow: 0 2px 20px 2px rgba(90, 90, 90, 0.47);
         z-index: 5;
-    }
-    .header-title{
-        padding: 10px;
-        flex:1;
-        text-align: left;
-    }
 
-    .header-title-text{
-        /* color: #fff; */
-        margin-bottom: 0;
-    }
+        .header-title {
+            padding: 10px;
+            flex: 1;
+            text-align: left;
+        }
 
-    .header-paticipants-text{
-        color: #e4e4e4;
-        font-size: 12px;
-        margin-top: 5px;
-        max-height: 30px;
-        overflow: hidden;
-    }
+        .header-title-text {
+            /* color: #fff; */
+            margin-bottom: 0;
+        }
 
-    .header-exit-button{
-        text-decoration: none;
-        color: #fff;
-        font-size: 20px;
-    }
+        .header-paticipants-text {
+            color: #e4e4e4;
+            font-size: 12px;
+            margin-top: 5px;
+            max-height: 30px;
+            overflow: hidden;
+        }
 
-    .icon-close-chat{
-        color:#fff;
-        width: 100%;
-    }
-    .icon-close-chat:hover{
-        color:rgb(238, 121, 121)
-    }
+        .header-exit-button {
+            text-decoration: none;
+            color: #fff;
+            font-size: 20px;
+        }
 
+        .icon-close-chat {
+            color: #fff;
+            width: 100%;
+        }
+
+        .icon-close-chat:hover {
+            color: rgb(238, 121, 121)
+        }
+    }
 </style>
